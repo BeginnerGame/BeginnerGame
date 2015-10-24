5e3b64ba43cd44d9869ddc0652d7963a6be665af
WilliamDT-Personal <WilliamDT-Personal@DESKTOP-TNBJP51>
2015-10-23 19:23:38 -0500

    Washing Dishes Program

--- src/Start.java
+++ src/Start.java
@@ -2,7 +2,19 @@
 public class Start {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		//https://github.com/BeginnerGame/BeginnerGame.git
+		
+		for(int i = 0; i < 5; i++)
+		{
+			System.out.println("Hello World!");
+		}
+		
+		int i = 0;
+		while(i < 5)
+		{
+			System.out.println("This does the same thing!");
+			i++;
+		}
 	}
 
 }
